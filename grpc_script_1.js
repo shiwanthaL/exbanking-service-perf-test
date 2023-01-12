96eece4e785ed5f784e7e1eb9b4c2bdb81eba626
Your Name <shivantha.msc@gmail.com>
2023-01-13 01:39:51 +0530

    initial strucure and baic rpc get-call adding

---
+++ grpc_script_1.js
@@ -0,0 +1,24 @@
+import grpc from "k6/net/grpc";
+import { sleep } from "k6";
+
+const client = new grpc.Client();
+client.load(["pb/proto/"], "banking_service.proto");
+
+export const options = {
+    vus: 5,
+    duration: "3s",
+};
+
+export default () => {
+    client.connect("localhost:9090", {
+        plaintext: true,
+    });
+
+    const req = { fullName: "" };
+    const response = client.invoke("BankingService/get_balance", req);
+
+    console.log(response.message);
+
+    client.close();
+    sleep(1);
+};
