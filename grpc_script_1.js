f82b856cf7c9aed806d434946eaede7479ae9314
Your Name <shivantha.msc@gmail.com>
2023-01-13 01:44:21 +0530

    proto file mapping with servre code

--- grpc_script_1.js
+++ grpc_script_1.js
@@ -2,7 +2,7 @@ import grpc from "k6/net/grpc";
 import { sleep } from "k6";
 
 const client = new grpc.Client();
-client.load(["pb/proto/"], "banking_service.proto");
+client.load(["pb/proto/"], "banking-service.proto");
 
 export const options = {
     vus: 5,
@@ -14,7 +14,7 @@ export default () => {
         plaintext: true,
     });
 
-    const req = { fullName: "" };
+    const req = { fullName: "Krashanth Josep" };
     const response = client.invoke("BankingService/get_balance", req);
 
     console.log(response.message);
